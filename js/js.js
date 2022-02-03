0d932cf6d361919c57675d944585118ebb2b6f81
Edilson Freire <edilsonmorais16@gmail.com>
2022-02-02 22:10:40 -0300

    Aplicando colisão no jogador e criando o movimento do inimigo1

--- js/js.js
+++ js/js.js
@@ -8,12 +8,32 @@ function start(){
 
     //Principais variáveis do Jogo
     var jogo = {}
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() * 334);
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    //Verifica se o usuario pressionou alguma tecla
+    $(document).keydown(function(e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e) {
+        jogo.pressionou[e.which] = false;
+    })
 
     //Game Loop
     jogo.timer = setInterval(loop,30);
-
+    
     function loop() {
         movefundo();
+        movejogador();
+        moveinimigo1();
     }
 
     function movefundo() {	
@@ -22,4 +42,53 @@ function start(){
         
     } // fim da função movefundo()
     
+    function movejogador(){
+        if(jogo.pressionou[TECLA.W]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo-10);
+
+            if(topo <= 0){
+                $("#jogador").css("top", topo+10);
+            }
+        }
+
+        if(jogo.pressionou[TECLA.S]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo+10);
+
+            if(topo >= 434){
+                $("#jogador").css("top", topo-10);
+            }
+        }
+        if(jogo.pressionou[TECLA.D]){
+            //chama funcao disparo
+        }
+    } // fim da funcao movejogador
+
+    /*
+    function moveinimigo1(){
+        posicaoX = parseInt($("#inimigo1").css("#left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if(posicaoX <= 0){
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
+    } */
+
+    function moveinimigo1() {
+
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left",posicaoX-velocidade);
+        $("#inimigo1").css("top",posicaoY);
+            
+        if (posicaoX<=0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+                
+        }
+    } //Fim da função moveinimigo1()
 }
